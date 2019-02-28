71827e0fe5df8e59a759e4e4553ff8dc9188e831
Louise Francois <louise.ban@gmail.com>
2019-02-28 21:45:31 +0100

    fixed PostList

--- homework-submission/homework2/todo/src/App.js
+++ homework-submission/homework2/todo/src/App.js
@@ -22,18 +22,6 @@ class App extends Component {
         <Message msg="Static List" />
         <List />
         <Message msg="Dynamic List" />
-        <tr className = "table">
-          <th className = "tHeading">Id</th>
-          <th className = "tHeading">Description</th>
-          <th className = "tHeading">Deadline</th>
-        </tr>
-        {PostData.map((postDetail, index)=>{
-          return <tr className ="table">
-                    <td className="tInput" >{postDetail.id}</td>
-                    <td className="tInput">{postDetail.description}</td>
-                    <td className="tInput">{postDetail.deadline}</td>
-                </tr>
-        })}
         <PostList  />
         </div>
       ); 
--- homework-submission/homework2/todo/src/posts/PostList.js
+++ homework-submission/homework2/todo/src/posts/PostList.js
@@ -5,7 +5,18 @@ class PostList extends Component {
     render() {
         return (
             <div>
-            
+                <tr className = "table">
+                    <th className = "tHeading">Id</th>
+                    <th className = "tHeading">Description</th>
+                    <th className = "tHeading">Deadline</th>
+                </tr>
+        {PostData.map((postDetail, index)=>{
+          return <tr className ="table">
+                    <td className="tInput" >{postDetail.id}</td>
+                    <td className="tInput">{postDetail.description}</td>
+                    <td className="tInput">{postDetail.deadline}</td>
+                </tr>
+        })}
             </div>
         )
     }
